424e72bd3d297c556674be37da17993d8d278b14
Jude Andersen <jude.ottilie@gmail.com>
2026-07-10 13:37:47 -0500

    post list, and added some to post manager

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -1,3 +1,73 @@
+import { useEffect, useState } from "react";
+import { getPosts } from "../../managers/postManager";
+import { getCategories } from "../../managers/categoryManager";
+import { getTags } from "../../managers/tagManager";
+import { Link } from "react-router-dom";
+import { Input, FormGroup, Label } from "reactstrap";
+
 export default function PostList() {
-  return <p>Post List</p>;
+  const [posts, setPosts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [selectedCategoryId, setSelectedCategoryId] = useState("");
+  const [selectedTagId, setSelectedTagId] = useState("");
+
+  useEffect(() => {
+    getCategories().then(setCategories);
+    getTags().then(setTags);
+  }, []);
+
+  useEffect(() => {
+    getPosts(selectedCategoryId || undefined, selectedTagId || undefined).then(
+      setPosts,
+    );
+  }, [selectedCategoryId, selectedTagId]);
+
+  return (
+    <div>
+      <p>Posts</p>
+
+      <FormGroup>
+        <Label>Filter by Category</Label>
+        <Input
+          type="select"
+          value={selectedCategoryId}
+          onChange={(e) => setSelectedCategoryId(e.target.value)}
+        >
+          <option value="">All Categories</option>
+          {categories.map((c) => (
+            <option key={c.id} value={c.id}>
+              {c.name}
+            </option>
+          ))}
+        </Input>
+      </FormGroup>
+
+      <FormGroup>
+        <Label>Filter by Tag</Label>
+        <Input
+          type="select"
+          value={selectedTagId}
+          onChange={(e) => setSelectedTagId(e.target.value)}
+        >
+          <option value="">All Tags</option>
+          {tags.map((t) => (
+            <option key={t.id} value={t.id}>
+              {t.name}
+            </option>
+          ))}
+        </Input>
+      </FormGroup>
+
+      <div>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <Link to={`/posts/${post.id}`}>{post.title}</Link>
+            <p>By {post.user?.fullName}</p>
+            <p>{post.category?.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/managers/postManager.js
+++ src/managers/postManager.js
@@ -1,20 +1,33 @@
 const _apiUrl = "/api/posts";
 
-export const getPosts = () => {
-    return fetch(_apiUrl).then((res) => res.json());
+export const getPosts = (categoryId, tagId) => {
+  let url = _apiUrl;
+  if (categoryId && tagId) {
+    url = `${_apiUrl}?categoryId=${categoryId}&tagId=${tagId}`;
+  } 
+  else if (categoryId) {
+    url = `${_apiUrl}?categoryId=${categoryId}`;
+  } 
+  else if (tagId) {
+    url = `${_apiUrl}?tagId=${tagId}`;
+  }
+  return fetch(url).then((res) => res.json());
 };
+
+
 
 export const getPostById = (id) => {
     return fetch(`${_apiUrl}/${id}`).then((res) => res.json());
 };
 
 export const getPostsByUserId = (userId) => {
-    return fetch(`${_apiUrl}/user/${userId}`).then((res) => res.json());
+    return fetch(`${_apiUrl}/myPosts/${userId}`).then((res) => res.json());
 };
 
 export const getPostsByTag = (tagId) => {
-    return fetch(`${_apiUrl}/tag/${tagId}`).then((res) => res.json());
+  return fetch(`${_apiUrl}?tagId=${tagId}`).then((res) => res.json());
 };
+
 
 export const createPost = (post) => {
     return fetch(_apiUrl, {
